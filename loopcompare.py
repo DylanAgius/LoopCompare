69f0202c705afbee7eade9660ef231ac89c543ee
Dylan Agius <dylan.j.agius@gmail.com>
2020-10-22 17:39:19 +0100

    added plotter

--- loopcompare.py
+++ loopcompare.py
@@ -8,12 +8,10 @@ import numpy as np
 import pandas as pd 
 import os
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
 import numpy.polynomial.polynomial as P
-import math
 
 
-class load_elinfo:
+class load_info:
    
     def __init__(self,cname):
         loc=os.path.join(os.path.dirname(__file__),'data')
@@ -25,10 +23,15 @@ class load_elinfo:
         self.exp=exp
         self.sim=sim
         
-    def fitcurves(self):
+    def fitcurves(self,**kwargs):
         
         exp=self.exp
         sim=self.sim
+        
+        #number of x points to use
+        xint=int(kwargs['xpoints'])
+        #number of polynomials
+        poly=int(kwargs['poly'])
         
         #determine if increasing values or decreasing ###need to have this for exp and sim
         decreas=[]
@@ -58,20 +61,12 @@ class load_elinfo:
                 turnpointexp.append(np.argmax(exp[:,pp]))
             if(decreassim[i]==False):
                 turnpointsim.append(np.argmax(sim[:,pp]))
-                #turnpointexp=np.asarray([x.index(max(x)) for x in zip(*exp[:,0::2])])
-                #turnpointsim=np.asarray([x.index(max(x)) for x in zip(*sim[:,0::2])])
             if(decreas[i]==True):
                 turnpointexp.append(np.argmin(exp[:,pp]))
             if(decreassim[i]==True):
                 turnpointsim.append(np.argmin(sim[:,pp]))
             pp+=2
-       # else:
-        #    turnpointexp=np.asarray([x.index(min(x)) for x in zip(*exp[:,0::2])])
-         #   turnpointsim=np.asarray([x.index(min(x)) for x in zip(*sim[:,0::2])])
-             
-        #find maximum and minimum strains
-       # for i in range(0,len(exp[0,0::2])):
-        #    maxv
+  
         maxvalsexp=np.asarray([max(x) for x in zip(*exp[:,0::2])])
         minvalsexp=np.asarray([min(x) for x in zip(*exp[:,0::2])])
 
@@ -86,25 +81,21 @@ class load_elinfo:
         p=2
         for i in range(0,len(exp[0,:])):
             if i % p == 0:
-               # zexp.append(P.polyfit(exp[:,i],exp[:,t],30))
-                zexp1.append(P.polyfit(exp[0:turnpointexp[n],i],exp[0:turnpointexp[n],t],4))
-                zexp2.append(P.polyfit(exp[turnpointexp[n]:,i],exp[turnpointexp[n]:,t],4))
-                zsim1.append(P.polyfit(sim[0:turnpointsim[n],i],sim[0:turnpointsim[n],t],4))
-                zsim2.append(P.polyfit(sim[turnpointsim[n]:,i],sim[turnpointsim[n]:,t],4))
+
+                zexp1.append(P.polyfit(exp[0:turnpointexp[n],i],exp[0:turnpointexp[n],t],poly))
+                zexp2.append(P.polyfit(exp[turnpointexp[n]:,i],exp[turnpointexp[n]:,t],poly))
+                zsim1.append(P.polyfit(sim[0:turnpointsim[n],i],sim[0:turnpointsim[n],t],poly))
+                zsim2.append(P.polyfit(sim[turnpointsim[n]:,i],sim[turnpointsim[n]:,t],poly))
             else:
                 t+=2
                 n+=1
    
         #new values and extra x-values
    
-        xpoints1=np.linspace(minvalsexp,maxvalsexp,100)
-        xpoints2=np.linspace(maxvalsexp,minvalsexp,100)
+        xpoints1=np.linspace(minvalsexp,maxvalsexp,xint)
+        xpoints2=np.linspace(maxvalsexp,minvalsexp,xint)
         
-       # plt.plot(exp[0:turnpointexp[1],0],exp[0:turnpointexp[1],1])
-        #plt.plot(exp[turnpointexp[1]:,0],exp[turnpointexp[1]:,1]) 
-        
-       # plt.plot(sim[0:turnpointexp[1],0],sim[0:turnpointexp[1],1])
-       # plt.plot(sim[turnpointexp[1]:,0],sim[turnpointexp[1]:,1])
+
         
         ffitexp1=[]
         ffitexp2=[]
@@ -116,25 +107,58 @@ class load_elinfo:
             ffitexp2.append(P.polyval(xpoints2[:,i], zexp2[i]))
             ffitsim1.append(P.polyval(xpoints1[:,i], zsim1[i]))
             ffitsim2.append(P.polyval(xpoints2[:,i], zsim2[i]))
-            
         
-        
-        plt.plot(xpoints1[:,0],ffitexp1[0],'r')
-        plt.plot(xpoints2[:,0],ffitexp2[0],'g')
-        
-        plt.plot(xpoints1[:,0],ffitsim1[0],'r')
-        plt.plot(xpoints2[:,0],ffitsim2[0],'g')
-        
-        plt.show()
-        
-     
         
         self.ffitexp1=np.asarray(ffitexp1)
         self.ffitsim1=np.asarray(ffitsim1)
         self.ffitexp2=np.asarray(ffitexp2)
         self.ffitsim2=np.asarray(ffitsim2)
+        self.xpoints1=xpoints1
+        self.xpoints2=xpoints2
         self.decreas=decreas
         self.decreassim=decreassim
+        self.stress=exp[:,1::2]
+        self.strain=exp[:,0::2]
+        self.stresssim=sim[:,1::2]
+        self.strainsim=sim[:,0::2]
+        
+    def plotter(self,**kwargs):
+        
+        #used to plot the supplied data with the fitted data
+        
+        #which loop to compare
+        interestloop=int(kwargs['interestloop'])-1
+        
+        ffitexp1=self.ffitexp1
+        ffitsim1=self.ffitsim1
+        ffitexp2=self.ffitexp2
+        ffitsim2=self.ffitsim2
+        xpoints1=self.xpoints1
+        xpoints2=self.xpoints2
+        stress=self.stress
+        strain=self.strain
+        stresssim=self.stresssim
+        strainsim=self.strainsim
+        
+        ffitsim=[[]]*len(ffitexp1[:,0])
+        ffitexp=[[]]*len(ffitexp1[:,0])
+        xpoints=[[]]*len(ffitexp1[:,0])
+        for i in range(0,len(ffitexp1[:,0])):
+            ffitexp[i]=np.append(ffitexp1[i,:],ffitexp2[i,:])
+            ffitsim[i]=np.append(ffitsim1[i,:],ffitsim2[i,:])
+            xpoints[i]=np.append(xpoints1[:,i],xpoints2[:,i])
+            
+        pltexpfit,=plt.plot(xpoints[interestloop],ffitexp[interestloop],'b',label='exp fit')
+        pltexp,=plt.plot(strain,stress,'b--',label='exp')
+        
+        pltsimfit,=plt.plot(xpoints[interestloop],ffitsim[interestloop],'r',label='sim fit')
+        pltsim,=plt.plot(strainsim,stresssim,'r--',label='sim')
+        
+        plt.legend(handles=[pltexpfit,pltexp,pltsimfit,pltsim])
+        
+        
+        plt.show()    
+        
         
     def errorcal(self):
         ffitexp1=self.ffitexp1
@@ -172,9 +196,7 @@ class load_elinfo:
             
         #calculate the unsigned relative error
         diff=(np.asarray(totalffitmax)-np.asarray(totalffitmin))/np.asarray(totalffitmin)
-        
-        
-        
+      
         #find the median unsigned percantage error
     
         MAPE=np.asarray([np.exp(np.median(np.abs(x)))-1 for x in zip(*np.transpose(ratio))])*100
@@ -187,6 +209,3 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return idx       
         
-load=load_elinfo('hysteresis')
-load.fitcurves()
-load.errorcal()
